a7dc6f9fa2a8d009b84b62cadc9863f803b30733
Tanguy Foujols <tanguy.foujols@icloud.com>
2018-02-28 15:46:10 +0100

    Autocomplete CSS ok, à ajuster avec le design de la home

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -6,7 +6,6 @@ const resultsFrom = document.querySelector('#results-from');
 const iata = document.querySelector('#iata');
 
 
-
 // On ecoute le clic sur la liste de resultats FROM
 resultsFrom.addEventListener('click', (event) => {
   // On renseigne la valeur dans le champ du formulaire
@@ -15,6 +14,7 @@ resultsFrom.addEventListener('click', (event) => {
   iataFrom.value = event.target.getAttribute('data-iota');
   // On efface la liste de resultats
   resultsFrom.innerHTML = '';
+  // resultsFrom.classList.remove("visible");
 });
 
 // On ecoute le clic sur la liste de resultats TO
@@ -22,13 +22,16 @@ resultsTo.addEventListener('click', (event) => {
   inputTo.value = event.target.innerText;
   iataTo.value = event.target.getAttribute('data-iota');
   resultsTo.innerHTML = '';
+  // resultsTo.classList.remove("visible");
 });
 
 
 
 // Results construction function
 const drawResponseList = (data, results) => {
-  results.innerHTML = '';
+  resultsTo.innerHTML = '';
+  resultsFrom.innerHTML = '';
+  // results.classList.add("visible");
   data.locations.forEach((location) => {
     let countryCode;
     let icon;
@@ -57,7 +60,7 @@ const drawResponseList = (data, results) => {
       country = "";
     };
     results.insertAdjacentHTML('beforeend',
-      `<li class="result" data-iota = "${iota}"> ${icon} <img src="http://www.countryflags.io/${countryCode}/flat/24.png"> ${location.name}, ${country}</li>`);
+      `<li class="result" data-iota = "${iota}"> <img src="http://www.countryflags.io/${countryCode}/flat/24.png"> <span class="location-name">${location.name}</span> <span class="country-name">${country}</span></li>`);
   });
 };
 
@@ -82,4 +85,27 @@ const autocompleteFrom = (e) => {
 // Autocomplete function call when search
 inputFrom.addEventListener('keyup', autocompleteFrom);
 inputTo.addEventListener('keyup', autocompleteTo);
+
+
+// Definition des top cities
+const topCities = [
+  ["Lisbonne", "LIS", "PT", "Portugal"],
+  ["Madrid", "MAD", "ES", "Espagne"],
+  ["Rome", "ROM", "IT", "Italie"],
+];
+
+
+inputTo.addEventListener('focus', function(){
+  resultsTo.innerHTML = '';
+  resultsFrom.innerHTML = '';
+  // Header destination populaire
+  resultsTo.insertAdjacentHTML('beforeend',
+  `<li class="top-cities"> <i class="fas fa-star"></i> Destinations populaires </li>`);
+  // Boucle sur l'array top cities
+  topCities.forEach((city) => {
+    resultsTo.insertAdjacentHTML('beforeend',
+      `<li class="result" data-iota = "${city[1]}"> <img src="http://www.countryflags.io/${city[2]}/flat/24.png"> <span class="location-name">${city[0]}</span> <span class="country-name">${city[3]}</span></li>`);
+  });
+});
+
 
