d824ea878649fe9e152a44ea79c6d7f554a6bd6f
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-02-28 18:04:10 +0100

    index-weekends-first-version-done

---
+++ url.rb
@@ -0,0 +1,9 @@
+https://api.skypicker.com/flights?flyFrom=PAR&to=MAD&dateFrom=03%2F03%2F2018&dateTo=03%2F03%2F2018&returnFrom=05%2F03%2F2018&returnTo=05%2F03%2F2018&directFlights=1&partner=picky&partner_market=us&limit=1
+
+https://api.skypicker.com/flights?flyFrom=PAR&to=MAD&dateFrom=10%2F03%2F2018&dateTo=10%2F03%2F2018&returnFrom=12%2F03%2F2018&returnTo=12%2F03%2F2018&directFlights=1&partner=picky&partner_market=us&limit=1
+
+https://api.skypicker.com/flights?flyFrom=PAR&to=MAD&dateFrom=17%2F03%2F2018&dateTo=17%2F03%2F2018&returnFrom=19%2F03%2F2018&returnTo=19%2F03%2F2018&directFlights=1&partner=picky&partner_market=us&limit=1
+
+https://api.skypicker.com/flights?flyFrom=PAR&to=MAD&dateFrom=24%2F03%2F2018&dateTo=24%2F03%2F2018&returnFrom=26%2F03%2F2018&returnTo=26%2F03%2F2018&directFlights=1&partner=picky&partner_market=us&limit=1
+
+https://api.skypicker.com/flights?flyFrom=PAR&to=MAD&dateFrom=31%2F03%2F2018&dateTo=31%2F03%2F2018&returnFrom=02%2F03%2F2018&returnTo=02%2F03%2F2018&directFlights=1&partner=picky&partner_market=us&limit=1
