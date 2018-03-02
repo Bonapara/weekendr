eba2d8ce130113e402135691512b2b6220ed5b87
Nathalie Roch <nathalie.m.roch@gmail.com>
2018-03-02 12:52:51 +0100

    lower price enhanced

---
+++ app/javascript/components/lowerPrice.js
@@ -0,0 +1,16 @@
+function lowerPrice() {
+  const prices = document.querySelectorAll(".price");
+  const array = [];
+  prices.forEach(function(element) {
+    const listPrices = element.innerText;
+    array.push(listPrices);
+  });
+  console.log(array.sort());
+  prices.forEach(function(element) {
+    if (element.innerText == array[0]) {
+      element.parentNode.classList.add("lower-price");
+    };
+  });
+}
+
+export { lowerPrice };
