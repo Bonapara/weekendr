6605c7580d1f49571adbed87d30b5b8fe2f6d5c2
Nathalie Roch <nathalie.m.roch@gmail.com>
2018-03-04 16:21:40 +0100

    card css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import "jquery-ui/ui/widgets/slider"
 import { slider } from '../components/slider';
 slider();
 import '../components/autocomplete';
+import { autocomplete } from '../components/lowerPrice';
+autocomplete();
 import '../components/lowerPrice';
 import { lowerPrice } from '../components/lowerPrice';
 lowerPrice();
