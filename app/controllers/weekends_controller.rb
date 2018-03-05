a3529972f7ed62d36d5d46b02ab824eb4e2b4e47
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-03-05 10:52:03 +0100

    filter adapted to 3 days- resolution of few bugs

--- app/controllers/weekends_controller.rb
+++ app/controllers/weekends_controller.rb
@@ -2,35 +2,10 @@ class WeekendsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @go_days = %w(Vendredi Samedi)
-    @return_days = %w(Dimanche Lundi)
     @format = params[:format]
     @code_from = params[:from]
     @code_to = params[:to]
 
-    # go day
-    # button Go day
-    if params[:go_day]
-      if params[:go_day] == 'Vendredi'
-        @go_day = 'Friday'
-      else
-        @go_day = 'Saturday'
-      end
-    else
-      @go_day = "Friday"
-    end
-
-    # return day
-    # button Return day
-    if params[:return_day]
-      if params[:go_day] == 'Dimanche'
-        @return_day = 'Sunday'
-      else
-        @return_day = 'Monday'
-      end
-    else
-      @return_day = "Sunday"
-    end
 
     # go time slide
     # from
@@ -72,9 +47,35 @@ class WeekendsController < ApplicationController
     end
 
 
-
-
     if params[:format] == "2"
+
+      @go_days = %w(Vendredi Samedi)
+      @return_days = %w(Dimanche Lundi)
+
+      # go day
+      # button Go day
+      if params[:go_day]
+        if params[:go_day] == 'Vendredi'
+          @go_day = 'Friday'
+        else
+          @go_day = 'Saturday'
+        end
+      else
+        @go_day = "Friday"
+      end
+
+      # return day
+      # button Return day
+      if params[:return_day]
+        if params[:go_day] == 'Dimanche'
+          @return_day = 'Sunday'
+        else
+          @return_day = 'Monday'
+        end
+      else
+        @return_day = "Sunday"
+      end
+
       @results = []
     # Renvoi vers initialize de api_response.rb
       input_attributes = {}
@@ -92,37 +93,48 @@ class WeekendsController < ApplicationController
 
     else
 
-      response1 = Wknd::ApiResponse.new(
-      "Thursday", # Jour aller
-      "Sunday", # Jour retour
-      {from: "18%3A00",to: "23%3A59"}, # Range heures aller
-      {from: "18%3A00",to: "23%3A59"}, # Range heures retour
-      "#{params[:code_from]}", # From
-      "#{params[:code_to]}") # To
+      @go_days = %w(Jeudi Vendredi Samedi)
+      @return_days = %w(Dimanche Lundi Mardi)
 
-      @results = response1.wknd_instances_creation
+      # go day
+      # button Go day
+      if params[:go_day]
+        if params[:go_day] == 'Jeudi'
+          @god_day = 'Thursday'
+        elsif params[:go_day] == 'Vendredi'
+          @go_day = 'Friday'
+        else
+          @go_day = 'Saturday'
+        end
+      else
+        @go_day = "Thursday"
+      end
 
-      ## code pour comparer deux formats de weekends
-      ## problème de décalage en raison de la date de réservation jeudi / vendredi
-      ## affaire à suivre
-      # response2 = Wknd::ApiResponse.new(
-      # "Friday", # Jour aller
-      # "Monday", # Jour retour
-      # {from: "18%3A00",to: "23%3A59"}, # Range heures aller
-      # {from: "18%3A00",to: "23%3A59"}, # Range heures retour
-      # "#{params[:code_from]}", # From
-      # "#{params[:code_to]}") # To
+      # return day
+      # button Return day
+      if params[:return_day]
+        if params[:go_day] == 'Dimanche'
+          @return_day = 'Sunday'
+        elsif params[:go_day] == 'Lundi'
+          @return_day = 'Monday'
+        else
+          @return_day = 'Tuesday'
+        end
+      else
+        @return_day = "Sunday"
+      end
 
-      # results2 = response2.wknd_instances_creation
+      input_attributes = {}
+      input_attributes[:go_day]             = @go_day                                       # Jour aller
+      input_attributes[:return_day]         = @return_day                                   # Jour retour
+      input_attributes[:go_hours_range]     = {from: @go_time_from, to: @go_time_to}        # Range heures retour
+      input_attributes[:return_hours_range] = {from: @return_time_from,to: @return_time_to} # Range heures retour
+      input_attributes[:city_from]          = @code_from                                    # From
+      input_attributes[:city_to]            = @code_to                                      # To
+      response = Wknd::ApiResponse.new(input_attributes)
 
-      # @results = []
-      # results1.each_with_index do |weekend1, index1|
-      #   if weekend1[:price] > results2[index1][:price]
-      #     @results << weekend1
-      #   else
-      #     @results << results2[index1]
-      #   end
-      # end
+      @results = response.wknd_instances_creation
+      render :index
     end
   end
 
