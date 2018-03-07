c0af9728de8bfbf96c5cd9013004c88efa066cca
Thomas CDF <tdesfrancs@gmail.com>
2018-03-07 09:50:44 +0100

    improve recap

--- app/javascript/components/linkCardsToRecap.js
+++ app/javascript/components/linkCardsToRecap.js
@@ -26,18 +26,33 @@ function cardObserver() {
 
 function linkCardsToRecap() {
   const cards = document.querySelectorAll('.card');
+  const timeLineDates = document.querySelectorAll('.time_line_date')
 
   cards.forEach(function(card) {
     card.addEventListener('mouseenter', (event) => {
-      const date = event.currentTarget.dataset.from;
+      const date = event.currentTarget.dataset.fromCard;
       const timelineDate = document.querySelector("[data-from='" + date + "']");
       timelineDate.classList.add("selected-date");
     });
 
     card.addEventListener('mouseleave', (event) => {
-      const date = event.currentTarget.dataset.from;
+      const date = event.currentTarget.dataset.fromCard;
       const timelineDate = document.querySelector("[data-from='" + date + "']");
       timelineDate.classList.remove("selected-date");
+    });
+  });
+
+  timeLineDates.forEach(function(timeLineDate) {
+    timeLineDate.addEventListener('mouseenter', (event) => {
+      const date = event.currentTarget.dataset.from;
+      const cardDate = document.querySelector("[data-from-card='" + date + "']");
+      cardDate.classList.add("selected-card");
+    });
+
+    timeLineDate.addEventListener('mouseleave', (event) => {
+      const date = event.currentTarget.dataset.from;
+      const cardDate = document.querySelector("[data-from-card='" + date + "']");
+      cardDate.classList.remove("selected-card");
     });
   });
 }
