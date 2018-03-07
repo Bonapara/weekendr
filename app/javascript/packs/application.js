cbf9e3cd33baeccd9e940d84055052a5a544b799
Tanguy Foujols <tanguy.foujols@icloud.com>
2018-03-07 20:59:51 +0100

    Filter design OK Filter backend OK with num_passenger parameter

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,17 @@
 import "bootstrap";
+import { slider } from '../components/slider';
 import { loadDynamicBannerText } from '../components/banner';
 import "jquery-ui/ui/widgets/slider"
-import { slider } from '../components/slider';
 import '../components/autocomplete';
 import { lowerPriceObserver } from '../components/lowerPrice';
 
-if (document.getElementById("banner-typed-text")) {
- loadDynamicBannerText();
+
+if (document.getElementById("weekends-container-row")) {
+ slider();
 }
 
-if (document.querySelector(".slider-time")) {
- slider();
+if (document.getElementById("banner-typed-text")) {
+ loadDynamicBannerText();
 }
 
 if (document.getElementById("weekends-container-row")) {
