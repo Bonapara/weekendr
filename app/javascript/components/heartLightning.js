3cab7db78047260be0715437891f67963570253d
Thomas CDF <tdesfrancs@gmail.com>
2018-03-08 13:11:25 +0100

    buttons of filters

--- app/javascript/components/heartLightning.js
+++ app/javascript/components/heartLightning.js
@@ -10,7 +10,6 @@ function heartObserver() {
   var callback = function(mutationsList) {
     for(var mutation of mutationsList) {
       if (mutation.type == 'childList') {
-        debugger;
         heartLightning();
       }
     }
