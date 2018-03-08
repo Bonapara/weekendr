de72fd76377f227b7e862ae5df304b309b86a78b
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-03-08 09:05:16 +0100

    ok

---
+++ app/controllers/list_users_controller.rb
@@ -0,0 +1,20 @@
+class ListUsersController < ApplicationController
+
+  def create
+    # require 'pry-byebug'
+    # binding.pry
+
+    # expéditeur : current_user
+    # bookmark > params
+    # destinataire > params depuis la form
+    # ListUser.new
+    # @list_user.destinataire = ...
+    # if .save
+    #   send mail...
+    # redirect_to
+    # else
+    #   fail path
+    # end
+  end
+
+end
